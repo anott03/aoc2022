d0223f129bb516234122c13013ed1cbbde6de111
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-01-09 00:41:51 -0500

    day 9 part 2

--- rust/src/bin/day9.rs
+++ rust/src/bin/day9.rs
@@ -64,8 +64,56 @@ fn main() {
             }
         });
 
-    // 2608 too small
-    // visited_points.iter().for_each(|point| println!("{:?}", point));
-    println!("Answer: {}", visited_points.len());
+    println!("Part 1: {}", visited_points.len());
+    println!("Time elapsed {}", start.elapsed().as_millis());
+
+    // cleanup
+    drop(tail);
+    drop(visited_points);
+    tails[0] = Point{x: 0, y: 0};
+    head = Point{x: 0, y: 0};
+
+    let mut visited_points: Vec<Point> = Vec::new();
+    visited_points.push(Point { x: 0, y: 0 });
+
+    contents
+        .strip_suffix("\n")
+        .unwrap()
+        .split("\n")
+        .for_each(|line| {
+            let mag = line[2..line.len()].parse::<i32>().unwrap();
+            let dir = &line[0..1];
+
+            for _ in 0..mag {
+                match dir {
+                    "U" => head.y += 1,
+                    "D" => head.y -= 1,
+                    "L" => head.x -= 1,
+                    "R" => head.x += 1,
+                    &_ => {}
+                }
+
+                let dx = head.x - tails[0].x;
+                let dy = head.y - tails[0].y;
+                if !is_touching(head, tails[0]) {
+                    tails[0].x += dx.signum();
+                    tails[0].y += dy.signum();
+                }
+
+                for i in 1..tails.len() {
+                    let dx = tails[i-1].x - tails[i].x;
+                    let dy = tails[i-1].y - tails[i].y;
+                    if !is_touching(tails[i-1], tails[i]) {
+                        tails[i].x += dx.signum();
+                        tails[i].y += dy.signum();
+                    }
+                }
+                if !visited_points.contains(&tails[8]) {
+                    visited_points.push(Point{ x: tails[8].x, y: tails[8].y });
+                }
+            }
+        });
+
+    println!("Part 2: {}", visited_points.len());
     println!("Time elapsed {}", start.elapsed().as_millis());
 }
